3efbd480de66846cbdd2fc305f8a8b5da795e5d4
Jabin Hao <1158608869seu@gmail.com>
2020-12-28 01:40:57 +0800

    逆向工程

---
+++ atcrowdfunding01-admin-parent/atcrowdfunding04-admin-entity/src/main/java/crowd/entity/Admin.java
@@ -0,0 +1,87 @@
+package crowd.entity;
+
+public class Admin {
+    private Integer id;
+
+    private String login;
+
+    private String userPswd;
+
+    private String userName;
+
+    private String email;
+
+    private String createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login == null ? null : login.trim();
+    }
+
+    public String getUserPswd() {
+        return userPswd;
+    }
+
+    public void setUserPswd(String userPswd) {
+        this.userPswd = userPswd == null ? null : userPswd.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime == null ? null : createTime.trim();
+    }
+
+    public Admin() {
+    }
+
+    public Admin(Integer id, String login, String userPswd, String userName, String email, String createTime) {
+        this.id = id;
+        this.login = login;
+        this.userPswd = userPswd;
+        this.userName = userName;
+        this.email = email;
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", userPswd='" + userPswd + '\'' +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", createTime='" + createTime + '\'' +
+                '}';
+    }
+}
